6f2b81bf8923625022ea904dcdc63dd6247a0c62
Pawan Kumar <pavan11896@gmail.com>
2024-11-07 03:52:34 +0530

    Updated

--- DjangoDevProject/urls.py
+++ DjangoDevProject/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('dev/', include('MaintainaceApp.urls')),
     path('dev/', include('GoogleTranslationApp.urls')),
     path('dev/', include('RedditPostsApp.urls')),
     path('dev/', include('YouTubeToolsApp.urls')),
