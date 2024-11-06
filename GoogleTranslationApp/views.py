522703ce18dd86518a5f50460f5ae83822af3606
Pawan Kumar <pavan11896@gmail.com>
2024-11-07 00:36:43 +0530

    Added Error Page

--- GoogleTranslationApp/views.py
+++ GoogleTranslationApp/views.py
@@ -7,26 +7,24 @@ def translate_text(request):
     if request.method == "POST":
         form = TranslateForm(request.POST)
         if form.is_valid():
-            text = form.cleaned_data['text']
-            target = form.cleaned_data['target_language'].title()
-            obj = Translator()
-            target = obj.language_to_code(target)
-            detected_language = obj.detect_language(text)
-            
-            if detected_language:
-                print(f"Source: {detected_language}") 
-                
-            translation,source = obj.translate(text,target)
-            
-            data = {
-                'detectedLanguage': detected_language,
-                'translatedText': translation,
-                'source': source,
-                'target' : obj.code_to_language(target)
-            }
-
-
-            return render(request, 'result.html',data)
+            try:
+                text = form.cleaned_data['text']
+                target = form.cleaned_data['target_language'].title()
+                obj = Translator()
+                target = obj.language_to_code(target)
+                detected_language = obj.detect_language(text)            
+                if detected_language:
+                    print(f"Source: {detected_language}")                 
+                translation,source = obj.translate(text,target)            
+                data = {
+                    'detectedLanguage': detected_language,
+                    'translatedText': translation,
+                    'source': source,
+                    'target' : obj.code_to_language(target)
+                }
+                return render(request, 'result.html',data)
+            except Exception as e:
+                return render(request, 'error_page.html', {'error_message': str(e)})
     else:
         form = TranslateForm()
 
