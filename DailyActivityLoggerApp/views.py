25559b2db228d8110ccd77e1151740403ea5da97
Pawan Kumar <pavan11896@gmail.com>
2024-11-07 01:16:52 +0530

    Updated

--- DailyActivityLoggerApp/views.py
+++ DailyActivityLoggerApp/views.py
@@ -8,19 +8,22 @@ def daily_activity_view(request):
     if request.method == "POST":
         form = DailyActivityForm(request.POST)
         if form.is_valid():
-            data = {
-                'date': form.cleaned_data['date'].strftime('%A, %d %b %Y'),
-                'learning': form.cleaned_data['learning'],
-                'meditation': form.cleaned_data['meditation'],
-                'physical_activity': form.cleaned_data['physical_activity'],
-                'sudoku': form.cleaned_data['sudoku'],
-                'reading_writing': form.cleaned_data['reading_writing'],
-                'comments': form.cleaned_data['comments'],
-            }
-            gs = GoogleSheetsAutomation()
-            ws = gs.select_worksheet(sheet_name="CUPAWAN", worksheet_index=0)
-            gs.insert_values(sheet = ws, append_row= list(data.values()))
-            return render(request, 'success.html')
+            try:
+                data = {
+                    'date': form.cleaned_data['date'].strftime('%A, %d %b %Y'),
+                    'learning': form.cleaned_data['learning'],
+                    'meditation': form.cleaned_data['meditation'],
+                    'physical_activity': form.cleaned_data['physical_activity'],
+                    'sudoku': form.cleaned_data['sudoku'],
+                    'reading_writing': form.cleaned_data['reading_writing'],
+                    'comments': form.cleaned_data['comments'],
+                }
+                gs = GoogleSheetsAutomation()
+                ws = gs.select_worksheet(sheet_name="CUPAWAN", worksheet_index=0)
+                gs.insert_values(sheet = ws, append_row= list(data.values()))
+                return render(request, 'success.html')
+            except Exception as e:
+                return render(request, 'error_page.html', {'error_message': str(e)})
 
     else:
         form = DailyActivityForm()
