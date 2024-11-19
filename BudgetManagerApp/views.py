30c1a5cb4a7fc04271857d9beb8f6a226ea8439a
Pawan Kumar <pavan11896@gmail.com>
2024-11-20 00:53:47 +0530

    Removed MongoDb Connection

--- BudgetManagerApp/views.py
+++ BudgetManagerApp/views.py
@@ -3,7 +3,6 @@ from .forms import FinanceForm
 from django.conf import settings
 from django.shortcuts import render
 from Scripts.Quotes import RandomQuote
-from Scripts.MongoDbUtils import MongoUtils
 from Scripts.GoogleSheetAutomation import GoogleSheetsAutomation
 
 
@@ -12,7 +11,6 @@ def log_view(request):
         form = FinanceForm(request.POST)
         if form.is_valid():
             try:
-                mongo_instance = MongoUtils()
                 data = {
                     'DateTime': form.cleaned_data.get('date', ).strftime('%d %b %Y, %H:%M'),
                     'How Much': form.cleaned_data['howmuch'],
@@ -22,7 +20,6 @@ def log_view(request):
                 gs = GoogleSheetsAutomation()
                 ws = gs.select_worksheet(sheet_name="DailyExpenseTracker", worksheet_index=0)
                 gs.insert_values(sheet = ws, append_row= list(data.values()))
-                mongo_instance.insert_records(data=data, collection_name= "budget_manager")
                 random_quote = RandomQuote().get_random_quote()
                 return render(request, 'BudgetManagerAppTemplates/Success.html', {'random_quote': random_quote})
             except Exception as e:
